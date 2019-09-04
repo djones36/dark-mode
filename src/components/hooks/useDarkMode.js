28467dfa9587d0fbb80b62ad384cd04b6cecae70
Daniel Jones <daniel.jones2000@gmail.com>
2019-09-04 16:19:06 -0700

    fixed error

--- src/components/hooks/useDarkMode.js
+++ src/components/hooks/useDarkMode.js
@@ -1,19 +1,14 @@
-import React from "react";
-import useLocalStorage from "./useLocalstorage";
+import { useEffect } from "react";
+import { useLocalStorage } from "./useLocalstorage";
 
 export const useDarkMode = intialValue => {
   const [darkMode, setDarkMode] = useLocalStorage("darkknight", intialValue);
   useEffect(() => {
-    useEffect(() => {
-      if (darkMode) {
-        document.body.classList.add("darkknight");
-      } else {
-        document.body.classList.remove("darkknight");
-      }
-    }, [darkMode]);
-    return () => {
-      cleanup;
-    };
-  }, [input]);
+    if (darkMode) {
+      document.body.classList.add("darkknight");
+    } else {
+      document.body.classList.remove("darkknight");
+    }
+  }, [darkMode]);
   return [darkMode, setDarkMode];
 };
