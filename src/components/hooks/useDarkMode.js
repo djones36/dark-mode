db470d7f96b8e5e3a70975dfc6cb81d992491b8b
Daniel Jones <daniel.jones2000@gmail.com>
2019-09-04 16:07:26 -0700

    working on darkmode function

--- src/components/hooks/useDarkMode.js
+++ src/components/hooks/useDarkMode.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import useLocalStorage from './useLocalstorage';
+import React from "react";
+import useLocalStorage from "./useLocalstorage";
 
-export const useDarkMode = () =>{
-    return(
-
-    )
-}
+export const useDarkMode = intialValue => {
+  const [darkMode, setDarkMode] = useLocalStorage("dark", intialValue);
+  if (darkMode){
+    document.body.classList.add('dark');
+  } else {
+      document.body.classList.remove('dark';)
+  }
+  return [darkMode, setDarkMode];
+};
